5015cdce1620078bb8e272df1a4d2e1db8a61db7
Funnydeveloper <funnydeveloper100@gmail.com>
2019-07-16 02:52:46 -0300

    add templates, tests case

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import homepageView
+from .views import homepageView, HomePageView, AboutPageView
 
 urlpatterns = [
-    path('', homepageView, name='home'),
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('', HomePageView.as_view(), name='home'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 def homepageView(request):
     return HttpResponse('Hello world!')
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
