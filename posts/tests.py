a4cb0e99df153abb829738e5138dfd1fcb64e58e
Funnydeveloper <funnydeveloper100@gmail.com>
2019-07-18 02:18:49 -0300

    first test success

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Post
+
+class PostModelTest(TestCase):
+    def setUp(self):
+        Post.objects.create(text='test post text')
+
+    def test_text_content(self):
+        post = Post.objects.get(id=1)
+        expected_object_name = f'{post.text}'
+        self.assertEqual(expected_object_name, 'test post text')
+
+    
