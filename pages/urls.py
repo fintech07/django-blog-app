ffe140ac48f6312460e91a3e19cb6ddf61590ee8
Funnydeveloper <funnydeveloper100@gmail.com>
2019-07-18 01:55:04 -0300

    change django version to 2.2

--- pages/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import homepageView, HomePageView, AboutPageView
-
-urlpatterns = [
-    path('about/', AboutPageView.as_view(), name='about'),
-    path('', HomePageView.as_view(), name='home'),
-]
