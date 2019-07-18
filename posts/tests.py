7a60754c20125ded643241c84a7890a3e34bdea2
Funnydeveloper <funnydeveloper100@gmail.com>
2019-07-18 04:35:46 -0300

    post detail view added

--- posts/tests.py
+++ posts/tests.py
@@ -1,31 +1,72 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
 from .models import Post
 
-class PostModelTest(TestCase):
-    def setUp(self):
-        Post.objects.create(text='test post text')
 
-    def test_text_content(self):
-        post = Post.objects.get(id=1)
-        expected_object_name = f'{post.text}'
-        self.assertEqual(expected_object_name, 'test post text')
-
-class HomePageViewTest(TestCase):
+class PostTests(TestCase):
 
     def setUp(self):
-        Post.objects.create(text='this is test one')
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            email='test@gmail.com',
+            password='secret'
+        )
 
-    def test_view_url_exists_at_proper_location(self):
-        resp = self.client.get('/')
-        self.assertEqual(resp.status_code, 200)
+        self.post = Post.objects.create(
+            title='A good title',
+            body='Nice body content',
+            author=self.user,
+        )
 
-    def test_view_url_by_name(self):
-        resp = self.client.get(reverse('home'))
-        self.assertEqual(resp.status_code, 200)
+    def test_string_representation(self):
+        post = Post(title='a sample title')
+        self.assertEqual(str(post), post.title)
 
-    def test_view_uses_correct_template(self):
-        resp = self.client.get(reverse('home'))
+    def test_post_content(self):
+        self.assertEqual(f'{self.post.title}', 'A good title')
+        self.assertEqual(f'{self.post.author}', 'testuser')
+        self.assertEqual(f'{self.post.body}', 'Nice body content')
+
+    def test_post_list_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Nice body content')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_post_detail_view(self):
+        resp = self.client.get('/post/1')
+        no_response = self.client.get('post/100000/')
         self.assertEqual(resp.status_code, 200)
-        self.assertTemplateUsed(resp, 'home.html')
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(resp, 'A good title')
+        self.assertTemplateUsed(resp, 'post_detail.html')
+
+# class PostModelTest(TestCase):
+#     def setUp(self):
+#         Post.objects.create(text='test post text')
+
+#     def test_text_content(self):
+#         post = Post.objects.get(id=1)
+#         expected_object_name = f'{post.text}'
+#         self.assertEqual(expected_object_name, 'test post text')
+
+# class HomePageViewTest(TestCase):
+
+#     def setUp(self):
+#         Post.objects.create(title='this is test one')
+
+#     def test_view_url_exists_at_proper_location(self):
+#         resp = self.client.get('/')
+#         self.assertEqual(resp.status_code, 200)
+
+#     def test_view_url_by_name(self):
+#         resp = self.client.get(reverse('home'))
+#         self.assertEqual(resp.status_code, 200)
+
+#     def test_view_uses_correct_template(self):
+#         resp = self.client.get(reverse('home'))
+#         self.assertEqual(resp.status_code, 200)
+#         self.assertTemplateUsed(resp, 'home.html')
+
--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 class HomePageView(ListView):
     model = Post
     template_name='home.html'
     context_object_name='all_posts_list'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name='post_detail.html'
 
 
