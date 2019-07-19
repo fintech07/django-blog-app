0ba6a1acb61b45a4d9e656e2671de9ceaeb040fc
Funnydeveloper <funnydeveloper100@gmail.com>
2019-07-19 03:14:48 -0300

    forms and user accounts:

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import HomePageView, PostDetailView
+from .views import HomePageView, PostDetailView, PostCreateView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', PostCreateView.as_view(), name='post_new'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 from .models import Post
 
 class HomePageView(ListView):
@@ -11,4 +12,8 @@ class PostDetailView(DetailView):
     model = Post
     template_name='post_detail.html'
 
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
 
